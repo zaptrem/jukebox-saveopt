279b330f46f9b61b1a08c7c9949789dd2dcbd445
zaptrem <zaptrem@me.com>
2022-08-10 16:24:40 -0400

    Revert "debug log and dont init distributed thingy tiwce"
    This reverts commit 4594bd29a4cac6485a005041680471a3ce948279.

--- jukebox/utils/dist_utils.py
+++ jukebox/utils/dist_utils.py
@@ -84,11 +84,7 @@ def _setup_dist_from_mpi(master_addr, backend, port, n_attempts, verbose):
     for attempt_idx in range(n_attempts):
         try:
             print("backend: " + backend)
-            if (not dist.is_initialized()):
-                print("dist not initialized, doing that now")
-                dist.init_process_group(backend=backend, init_method=f"env://")
-            else:
-                print("dist already initialized")
+            dist.init_process_group(backend=backend, init_method=f"env://")
             assert dist.get_rank() == mpi_rank
 
             use_cuda = False #torch.cuda.is_available()
